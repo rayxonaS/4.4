6ff4d81a123c3c322203c3424c8d3349c4629a10
rayxona <rayxona@gmail.com>
2025-01-17 21:25:26 +0500

    initial commit: add HTML structure and JavaScript logic for boolean checks

---
+++ app.js
@@ -0,0 +1,210 @@
+//boolean 1
+
+//let a = +prompt("Son kiriting");
+//if (a > 0) {
+//console.log("A soni musbat son");
+//} else {
+// console.log("A soni musbat son emas");
+//}
+
+//boolean 2
+
+//let a = +prompt("Son kiriting");
+//if (a % 2 == 1) {
+//console.log("A soni toq son");
+//} else {
+//  console.log(" A soni  toq son emas");
+//}
+
+//boolean 3
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//if (A > 2 && B <= 3) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 4
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//let C = +prompt("C sonini kiriting");
+//if (A <= B && B <= C) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 5
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//if ((A % 2 == 0 && B % 2 == 0) || (A % 2 == 1 && B % 2 == 1)) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 6
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//let C = +prompt("C sonini kiriting");
+//if (
+//(A > 0 && B > 0 && C < 0) ||
+//(A > 0 && B < 0 && C > 0) ||
+//(A < 0 && B > 0 && C > 0)
+//) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 7
+
+//let son = +prompt("Uch xonali son kiriting");
+//let A = (son - (son % 100)) / 100;
+//let B = ((son % 100) - (son % 10)) / 10;
+//let C = son % 10;
+//if (A != B && B != C) {
+//console.log("Juma rost");
+//} else {
+//console.log("Jumal rost emas");
+//}
+
+//boolean 8
+
+//let son = +prompt("Uch xonali son kiriting");
+//let A = (son - (son % 100)) / 100;
+//let B = ((son % 100) - (son % 10)) / 10;
+//let C = son % 10;
+//if (A == B || B == C || A == C) {
+//console.log("Juma rost");
+//} else {
+//console.log("Jumal rost emas");
+//}
+
+//boolean 9
+
+//let a = +prompt("Son kiriting");
+//if (a % 2 == 0) {
+//console.log("A soni juft son");
+//} else {
+//console.log("A soni juft son emas");
+//}
+
+//boolean 10
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//if (A >= 0 || B < -2) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 11
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//let C = +prompt("C sonini kiriting");
+//if (A < B && B < C) {
+//console.log("Jumla rost");
+//} else {
+// console.log("Jumla rost emas");
+//}
+
+//boolean 12
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//if (A % 2 == 1 &7 B % 2 == 1) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 13
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//if (A % 2 == 1 && B % 2 == 0 || A % 2 == 0 && B % 2 ==1 ) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 14
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//if (A % 2 == 1 &7 B % 2 == 0) {
+//console.log("Jumla rost");
+//} else {
+//console.log("Jumla rost emas");
+//}
+
+//boolean 15
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//let C = +prompt("C sonini kiriting");
+//if (A > 0 && B > 0 && C > 0 ) {
+//console.log("Jumla rost");
+//} else {
+// console.log("Jumla rost emas");
+//}
+
+//boolean 16
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//let C = +prompt("C sonini kiriting");
+//if (A > 0 && B < 0 && C < 0) {
+//console.log("Jumla rost");
+//} else {
+// console.log("Jumla rost emas");
+//}
+
+//boolean 17
+
+//let A = +prompt("A sonini kiriting");
+//let B = +prompt("B sonini kiriting");
+//let C = +prompt("C sonini kiriting");
+//if (A < 0 && B > 0 && C > 0) {
+//console.log("Jumla rost");
+//} else {
+// console.log("Jumla rost emas");
+//}
+
+//boolean 18
+
+//let son = 45;
+//if ( son > 0 && son > 9 && son <= 99 && son % 2 == 0) {
+//console.log("Ikki xonali juft son");
+//} else {
+//console.log("Ikki xonali juft son emas");
+//}
+
+//boolean 19
+
+//let son = 456;
+//if ( son > 0 && son > 99 && son <= 999 && son % 2 == 1) {
+//console.log("Uch  xonali toq son");
+//} else {
+// console.log("Uch xonali toq son emas");
+//}
+
+//boolean 20
+
+//let son = +prompt("Uch xonali son kiriting");
+//let A = (son - (son % 100)) / 100;
+//let B = ((son % 100) - (son % 10)) / 10;
+//let C = son % 10;
+//if ((A !== B && B == C) || (A == B && B !== C) || (A == C && B !== C)) {
+//console.log("Juma rost");
+//} else {
+//console.log("Jumal rost emas");
+//}
